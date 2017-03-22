4f25456c9909582f05d81b8ebe457e7fc97facfb
maokuo <352785171@qq.com>
2017-03-22 14:59:56 +0800

    2017-03-22 add angular-chart

--- scripts/controllers/AboutController.js
+++ scripts/controllers/AboutController.js
@@ -8,9 +8,39 @@ define(['angular', 'require'], function (angular, require) {
 
     function AboutController($scope, $ocLazyLoad) {
 
-        $scope.controllerName = "AboutController";
+        var controllerName="AboutController";
+        $scope.controllerName = controllerName;
 
         var vm = this;
-        vm.controllerName = "AboutController";
+        vm.controllerName = controllerName;
+
+        vm.injectDemo= function () {
+
+            //创建一个新的模块
+            var app =angular.module('app',[]);
+            app.controller('TempController', function ($scope) {
+                $scope.controllerName='TempController';
+            });
+
+            var injector=angular.injector(['ng','app']),
+                $controller=injector.get('$controller'),
+                scope = injector.get('$rootScope').$new();
+            $controller('TempController',{$scope:scope});
+
+            require(['jquery'], function ($) {
+                var $div = $('<div ng-controller="TempController">{{controllerName}}</div>');
+                $('#containerBox').append($div);
+
+                injector.invoke(function ($compile) {
+                    $scope.$apply(function () {
+                        var scope = angular.element($div).scope();
+                        $compile($div)(scope);
+                    })
+
+                });
+            });
+
+
+        };
     }
 })
